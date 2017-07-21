5b0760a368fb95a1bedf431eac727c52972b896f
VVCepheiA <oyevera@gmail.com>
2017-07-21 13:38:30 -0400

    refactor

--- pick_selection.py
+++ pick_selection.py
@@ -1,7 +1,14 @@
 import random
+import os
 
-txt = open('../corpus.txt')
-lines = txt.readlines()
-start = random.randrange(0,len(lines))
+def pick(rel_path="human.txt"):
+	script_path = os.path.abspath(__file__)
+	script_dir = os.path.split(script_path)[0]
+	abs_file_path = os.path.join(script_dir, rel_path)
 
-print("".join(lines[start:(start+12)]))
+	txt = open(abs_file_path)
+	lines = txt.readlines()
+	start = random.randrange(0,len(lines))
+	start -= start % 4
+
+	print("".join(lines[start:(start+4)]))
--- pick_selection_rnn.py
+++ pick_selection_rnn.py
@@ -1,14 +1,3 @@
-import random
-import os
+from pick_selection import pick
 
-script_path = os.path.abspath(__file__)
-script_dir = os.path.split(script_path)[0]
-rel_path = "human.txt"
-abs_file_path = os.path.join(script_dir, rel_path)
-
-txt = open(abs_file_path)
-lines = txt.readlines()
-start = random.randrange(0,len(lines))
-start -= start % 4
-
-print("".join(lines[start:(start+4)]))
+pick("rnn.txt")
